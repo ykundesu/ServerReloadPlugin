0ec4046843f16b6479058378b3cacad9e26afa63
ykundesu <49261069+ykundesu@users.noreply.github.com>
2021-01-23 19:20:40 +0900

    Update Serverreload.java

--- main/java/jp/yoppykun/serverreload/serverreload/Serverreload.java
+++ main/java/jp/yoppykun/serverreload/serverreload/Serverreload.java
@@ -35,9 +35,6 @@ public final class Serverreload extends JavaPlugin {
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kick @a サーバーを再起動しています。");
                 }
 
-                    //BukkitTask task = null;
-                   // task = this.getServer().getScheduler().runTaskTimer(this, new Timer(this,5), 0L, 1200L); // 10秒(1万ミリ秒)間だけ処理を止める
-
 
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "reload confirm");
             } else if (args[0].equalsIgnoreCase("stop")){
@@ -46,8 +43,7 @@ public final class Serverreload extends JavaPlugin {
                 } else {
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kick @a サーバを停止しています。");
                 }
-                //BukkitTask task = null;
-                //task = this.getServer().getScheduler().runTaskTimer(this, new Timer(this ,5), 0L, 1200L);
+
 
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
             } else {sender.sendMessage("引数が違います:reload,stop");}
@@ -63,11 +59,11 @@ public final class Serverreload extends JavaPlugin {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (!command.getName().equalsIgnoreCase("sreload")) return super.onTabComplete(sender, command, alias, args);
         if (args.length == 1) {
-            if (args[0].length() == 0) { // /testまで
+            if (args[0].length() == 0) {
                 return Arrays.asList("reload", "stop");
             } else {
                 
-                //入力されている文字列と先頭一致
+
                 if ("reload".startsWith(args[0])) {
                     return Collections.singletonList("reload");
                 } else if ("stop".startsWith(args[0])) {
@@ -75,36 +71,7 @@ public final class Serverreload extends JavaPlugin {
                 }
             }
         }
-        //JavaPlugin#onTabComplete()を呼び出す
+
         return super.onTabComplete(sender, command, alias, args);
     }
-        //JavaPlugin#onTabComplete()を呼び出す
-        /*private class Timer extends BukkitRunnable{
-            int time;//秒数
-            JavaPlugin plugin;//BukkitのAPIにアクセスするためのJavaPlugin
-            public Timer(JavaPlugin plugin ,int i) {
-                this.time = i;
-                this.plugin = plugin;
-            }
-            @Override
-            public void run() {
-                if(time <= 0){
-                }else{
-                    time--;
-                }
-                //1秒減算
-            }
-            public void onJoin(PlayerJoinEvent event) {
-                // 20ticks後に1度だけ実行される処理を、実装しつつ、そのままスケジュールします。
-
-                new BukkitRunnable() {
-
-                    @Override
-                    public void run() {
-                        // スケジューラで実行される処理内容を、ここに実装します。
-                        getServer().broadcastMessage("サーバーへようこそ！説明文をちゃんと読んでね！");
-                    }
-                }.runTaskLater((Plugin) this,20);
-                // ↑そのままスケジュールします。
-            }}*/
     }
